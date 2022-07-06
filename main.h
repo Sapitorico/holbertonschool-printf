46ca696f0f00ae718868beb639817772a5c4e3f0
Sapitorico <renzoyaques@gmail.com>
2022-07-06 08:58:06 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/*PROTOTYPES*/
+typedef struct f_types
+{
+	char types;
+	int (*f)(va_list arg);
+} f_types_t;
+
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+
+/*PROTOTPEYS FUNCTIONS*/
+
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_indic(va_list arg);
+
+#endif /*MAIN_H*/
