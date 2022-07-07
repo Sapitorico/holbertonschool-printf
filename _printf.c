ef806c3e6038a24ca87a6b0a7b8754e2da53d814
Sapitorico <renzoyaques@gmail.com>
2022-07-07 11:14:26 -0700

    fix betty

--- _printf.c
+++ _printf.c
@@ -5,12 +5,12 @@ int _printf(const char *format, ...)
 	int const1 = 0, const2 = 0;
 	int (*f)(va_list arg);
 
-	va_start(args, format);         /*initialize arguments list*/
+	va_start(args, format);		/*initialize arguments list*/
 	if (!format)
 		return (-1);
-	while (format[const1] != '\0')  /*check and recognize the length of format*/
+	while (format[const1] != '\0')	/*check and recognize the length of format*/
 	{
-		for (; format[const1] != '%' && format[const1]; const1++)       /*recognize and position in the % indicator*/
+		for (; format[const1] != '%' && format[const1]; const1++)	/*recognize and position in the % indicator*/
 		{
 			_putchar(format[const1]);
 			const2++;
@@ -23,7 +23,7 @@ int _printf(const char *format, ...)
 		if (f != NULL)
 		{
 			const2 += f(args);
-			const1 += 2;    /*print the following characters*/
+			const1 += 2;	/*print the following characters*/
 			continue;
 		}
 		if (!format[const1 + 1])
@@ -31,12 +31,12 @@ int _printf(const char *format, ...)
 			return (-1);
 		}
 		_putchar(format[const1]);
-		const2++;       /*print the string*/
+		const2++;	/*print the string*/
 		if (format[const1 + 1] == '%')
 			const1 += 2;
 		else
 		const1++;
 	}
 	va_end(args);
-	return (const2);        /*Returns the number of characters printed*/
+	return (const2);	/*Returns the number of characters printed*/
 }
