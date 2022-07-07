9320ebbda572e281b8ff134a2ca24a0a9d8eb19c
Sapitorico <renzoyaques@gmail.com>
2022-07-07 11:01:10 -0700

    fix betty

--- _printf.c
+++ _printf.c
@@ -56,28 +56,3 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (const2);        /*Returns the number of characters printed*/
 }
-  /*format functions*/
-int print_char(va_list arg)
-{
-	char character;
-
-	character = va_arg(arg, int);
-	_putchar(character);
-	return (1);
-}
-int print_string(va_list arg)
-{
-	char *str;
-	int i = 0;
-
-	str = va_arg(arg, char *);
-	if (str == NULL)
-	{
-		str = "(null)";
-	}
-	for (; str[i]; i++)
-	{
-		_putchar(str[i]);
-	}
-	return (i);
-}
