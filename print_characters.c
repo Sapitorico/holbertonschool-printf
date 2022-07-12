5f6d43026705b5e28fd25e1b4e56250489f66874
rrtyui <marcelo6549@gmail.com>
2022-07-12 06:34:21 -0700

    bettyfix

--- print_characters.c
+++ print_characters.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_char - function that prints characters
- *
+ *@arg: argument
  * Return: always 1
  */
 int print_char(va_list arg)
 {
-        char character;
+	char character;
 
-        character = va_arg(arg, int);
-        _putchar(character);
-        return (1);
+	character = va_arg(arg, int);
+	_putchar(character);
+	return (1);
 }
--- print_numbers.c
+++ print_numbers.c
@@ -1,54 +1,55 @@
 #include "main.h"
+
 /**
  * print_int - función que imprime numeros enteros
- *
+ *@arg: parametrer
  * Return: retorna al contador inicial
  */
 int print_int(va_list arg)
 {
-        long int num;
+	long int num;
 	long int i = 1;
 	int cont = 0;
 
-        num = va_arg(arg, int);
+	num = va_arg(arg, int);
 
-        if (num == 0)
-        {
-                _putchar('0');
-                return (i);
-        }
-        if (num < 0)
-        {
-                _putchar('-');
-                cont++;
-                num = num *-1;
-        }
-        for (;i <= num; i *= 10)
-                cont++;
-        i /= 10;
-        for(; i > 0; i /= 10)
-        {
-                _putchar(((num / i) % 10) + '0');
-        }
-        return (cont);
+	if (num == 0)
+	{
+		_putchar('0');
+		return (i);
+	}
+	if (num < 0)
+	{
+		_putchar('-');
+		cont++;
+		num = (num * -1);
+	}
+	for (; i <= num; i *= 10)
+		cont++;
+	i /= 10;
+	for (; i > 0; i /= 10)
+	{
+		_putchar(((num / i) % 10) + '0');
+	}
+	return (cont);
 }
 /**
  * print_decimal - función que imprime numeros decimales
- *
- * Retur: retorna a la función int llamando a esta función
+ *@arg: parametrer
+ * Return: retorna a la función int llamando a esta función
  */
 int print_decimal(va_list arg)
 {
-        return (print_int(arg));
+	return (print_int(arg));
 }
 /**
  * print_unsig_deci- function that prints unsigned integers
- *
+ *@arg: parametrer
  * Return: counter
  */
 int print_unsig_deci(va_list arg)
 {
-        unsigned int num;
+	unsigned int num;
 	unsigned int i = 1;
 	int cont = 0;
 
@@ -58,12 +59,12 @@ int print_unsig_deci(va_list arg)
 		_putchar('0');
 		return (i);
 	}
-	for (;i <= num; i *= 10)
+	for (; i <= num; i *= 10)
 		cont++;
 	i /= 10;
-	for(; i > 0; i /= 10)
+	for (; i > 0; i /= 10)
 	{
 		_putchar(((num / i) % 10) + '0');
 	}
-	return (cont); 
+	return (cont);
 }
