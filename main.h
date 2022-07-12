bcdfd6f319f340be7dc6787095e4f319ff38d243
rrtyui <marcelo6549@gmail.com>
2022-07-12 05:59:23 -0700

    fix

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
 /*PROTOTYPES*/
 /**
  * struct f_types - format struct
@@ -14,8 +15,8 @@
  */
 typedef struct f_types
 {
-	char types;
-	int (*f)(va_list arg);
+        char types;
+        int (*f)(va_list arg);
 } f_types_t;
 
 int _putchar(char c);
@@ -23,10 +24,16 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 
 /*PROTOTPEYS FUNCTIONS*/
-int (*get_format(const char *format))(va_list arg);
+int (*get_formatted_funct(const char *format))(va_list arg);
 
 int print_char(va_list arg);
 int print_string(va_list arg);
 int print_int(va_list arg);
+int print_decimal(va_list arg);
+int print_unsig_deci(va_list arg);
+/*int print_octa(va_list arg);*/
+int print_strev(va_list arg);
 
+/*int _convert(unsigned int num, int base);*/
+int str_len(const char *str);
 #endif /*MAIN_H*/
