81577d3da758c2613d783663ab5037e59e7a52a2
rrtyui <marcelo6549@gmail.com>
2022-07-12 06:58:04 -0700

    bettfix

--- main.h
+++ main.h
@@ -11,12 +11,12 @@
  * struct f_types - format struct
  * @types: struct
  * @f: pinter to formats
- * @arg: formats
  */
 typedef struct f_types
 {
-        char types;
-        int (*f)(va_list arg);
+	char types;
+	int (*f)(va_list arg);
+
 } f_types_t;
 
 int _putchar(char c);
@@ -31,9 +31,6 @@ int print_string(va_list arg);
 int print_int(va_list arg);
 int print_decimal(va_list arg);
 int print_unsig_deci(va_list arg);
-/*int print_octa(va_list arg);*/
 int print_strev(va_list arg);
-
-/*int _convert(unsigned int num, int base);*/
 int str_len(const char *str);
 #endif /*MAIN_H*/
