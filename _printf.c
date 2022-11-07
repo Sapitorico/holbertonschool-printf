38328cf31ae673c81bc1b432ca82471db7fa46a3
Sapitorico <renzoyaques@gmail.com>
2022-11-07 08:07:57 -0800

    First Functional Version

--- _printf.c
+++ _printf.c
@@ -19,8 +19,6 @@ int _printf(const char *format, ...)
 			_putchar(format[constant]);
 			counter++;
 		}
-		/**printf("position char:[%c]\n", format[constant]);**/
-		/**printf("position num:[%d]\n", counter);**/
 		if (format[constant] == '%')
 		{
 			constant++;
@@ -35,43 +33,3 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (counter);
 }
-
-/**
- * previous code - changes
- * printed well in some cases
- * errors:
- * - did not accept multiple formats
- * - took one character less than the long on
- *
- */
-/**	while (format[constant])
-	{
-		for (; format[constant] != '%' && format[constant]; constant++)
-		{
-			_putchar(format[constant]);
-			counter++;
-		}
-		*unnecessary exit condition*
-		*conditioned by the while*
-		if (!format[constant])
-			return (counter);
-		*format positioned in %, I made a check condition and incremented the constant within*
-		f = get_format(&format[constant + 1]);
-		if (f)
-		{
-			counter += f(args);
-			constant += 2;
-			continue;*execution resumes at the top of the loop with the evaluation of the loop test*
-		}
-		if (!format[constant + 1])
-			return (-1);
-		_putchar(format[constant]);
-		constant++;
-		if (format[constant + 2] == '%')
-			counter += 2;
-		else
-			constant++;
-	}
-	*here limit the argument list, no longer used*
-	return (counter);
-**/
